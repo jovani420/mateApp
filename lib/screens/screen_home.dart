100cd38a22910b4f0a9c1c27ef35debd77b06bd7
jovani <jovi.ops78@gmail.com>
2024-12-15 14:20:12 -0700

    Se agrego metodo ListView

--- lib/screens/screen_home.dart
+++ lib/screens/screen_home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:materia_app/widgets/listcardfeed.dart';
 
 class ScreenHome extends StatelessWidget {
   const ScreenHome({
@@ -13,56 +14,7 @@ class ScreenHome extends StatelessWidget {
         foregroundColor: const Color.fromARGB(255, 249, 249, 249),
         backgroundColor: Colors.green,
       ),
-      body: Container(
-        margin: const EdgeInsets.only(top: 10),
-        width: double.infinity,
-        height: 350,
-        decoration: const BoxDecoration(color: Colors.white),
-          child: Column (
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                padding: const EdgeInsets.all(10),
-                child: const Row(
-                  children: [
-                    CircleAvatar(child: Text("J")),
-                    SizedBox(width: 10),
-                    Text(
-                      "Jovani Maytorena",
-                      style: TextStyle(
-                          color: Color.fromARGB(255, 83, 83, 83),
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold),
-                    )
-                  ],
-                ),
-              ),
-              const Padding(
-                padding:  EdgeInsets.only(left: 10, right: 10, bottom: 10),
-                child: Text("Este es una imagen descriptiva, acerca de la conexion es de internet por medio de ip, en la cual se enlazan por paquetes de datos",
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis, style: TextStyle(color: Colors.grey)),
-              ),
-              Container (
-                width: double.infinity,
-                height: 200,
-                child: Image.network("https://www.adslzone.net/app/uploads-adslzone.net/2021/12/www-vs-internet-800x419.jpg",
-              fit: BoxFit.cover,
-              ),
-              ),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                TextButton(onPressed: (){}, child: const Text("Me gusta", style: TextStyle(fontSize: 9.5,    fontWeight: FontWeight.bold, color: Colors.grey))),
-                TextButton(onPressed: (){}, child: const Text("Comentario", style: TextStyle(fontSize: 9.5 , fontWeight: FontWeight.bold, color: Colors.grey))),
-                TextButton(onPressed: (){}, child: const Text("Compartir" , style: TextStyle(fontSize: 9.5 , fontWeight: FontWeight.bold, color: Colors.grey)))
-                ],
-              ),
-            ),
-            ],
-          ),
-        ),
+      body: const ListCardFeed(),
       backgroundColor: Colors.grey[320],
     );
   }
