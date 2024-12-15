7f27147e806bc9138f2ccf417c72611d08e35273
jovani <jovi.ops78@gmail.com>
2024-12-15 09:41:43 -0700

    Se le agrego al text descritivo un overflow.ellipsis

--- lib/screens/screen_home.dart
+++ lib/screens/screen_home.dart
@@ -18,7 +18,8 @@ class ScreenHome extends StatelessWidget {
         width: double.infinity,
         height: 350,
         decoration: const BoxDecoration(color: Colors.white),
-          child: Column(
+          child: Column (
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Container(
                 padding: const EdgeInsets.all(10),
@@ -36,7 +37,13 @@ class ScreenHome extends StatelessWidget {
                   ],
                 ),
               ),
-              Container(
+              const Padding(
+                padding:  EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                child: Text("Este es una imagen descriptiva, acerca de la conexion es de internet por medio de ip, en la cual se enlazan por paquetes de datos",
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis, style: TextStyle(color: Colors.grey)),
+              ),
+              Container (
                 width: double.infinity,
                 height: 200,
                 child: Image.network("https://www.adslzone.net/app/uploads-adslzone.net/2021/12/www-vs-internet-800x419.jpg",
@@ -47,9 +54,9 @@ class ScreenHome extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                TextButton(onPressed: (){}, child: const Text("Me gusta", style: TextStyle(fontSize: 10.5,    fontWeight: FontWeight.bold))),
-                TextButton(onPressed: (){}, child: const Text("Comentario", style: TextStyle(fontSize: 10.5 , fontWeight: FontWeight.bold))),
-                TextButton(onPressed: (){}, child: const Text("Compartir" , style: TextStyle(fontSize: 10.5 , fontWeight: FontWeight.bold)))
+                TextButton(onPressed: (){}, child: const Text("Me gusta", style: TextStyle(fontSize: 9.5,    fontWeight: FontWeight.bold, color: Colors.grey))),
+                TextButton(onPressed: (){}, child: const Text("Comentario", style: TextStyle(fontSize: 9.5 , fontWeight: FontWeight.bold, color: Colors.grey))),
+                TextButton(onPressed: (){}, child: const Text("Compartir" , style: TextStyle(fontSize: 9.5 , fontWeight: FontWeight.bold, color: Colors.grey)))
                 ],
               ),
             ),
